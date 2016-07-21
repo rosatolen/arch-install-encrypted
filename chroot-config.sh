30649cb827676028f93c047ed584090056632dae
Rosalie Tolentino <rosatolen@thoughtworks.com>
2016-07-21 14:06:25 -0500

    ordering fix

--- chroot-config.sh
+++ chroot-config.sh
@@ -15,6 +15,8 @@ echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc --utc
 
+pacman -S grub os-prober
+
 ##############################################
 # Open the /etc/default/grub file and add:
 # cryptdevice=/dev/sda2:base root=/dev/mapper/base resume=/dev/mapper/base
@@ -22,10 +24,10 @@ hwclock --systohc --utc
 
 # TODO: automate this step by replacing existing GRUB line that starts with GRUB_CMDLINE_LINUX_DEFAULT with the following variable:
 DRIVE_ENCRYPTION_OPTIONS="GRUB_CMDLINE_LINUX_DEFAULT=\"quiet cryptdevice=""$ENCRYPTED_DRIVE_PARTITION"":base root=/dev/mapper/base resume=/dev/mapper/base\""
+vi /etc/default/grub
 
 ##############################################
-pacman -S grub os-prober
-vi /etc/default/grub
+
 grub-install --recheck $BOOT_DRIVE
 grub-mkconfig -o /boot/grub/grub.cfg
 
